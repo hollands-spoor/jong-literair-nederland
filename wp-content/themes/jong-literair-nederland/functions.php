95a4980886ffebc2b4804d65d1df7c961aa363b4
hollands-spoor <github_hs@hollands-spoor.nl>
2026-05-20 13:51:03 +0200

    Removed parent theme

--- wp-content/themes/jong-literair-nederland/functions.php
+++ wp-content/themes/jong-literair-nederland/functions.php
@@ -4,20 +4,14 @@ add_action( 'wp_enqueue_scripts', 'jln_child_theme_enqueue_assets' );
 add_action( 'admin_enqueue_scripts', 'jln_child_theme_enqueue_admin_assets' );
 add_action( 'after_setup_theme', 'jln_theme_setup_editor_styles' );
 /**
- * Load parent + child theme assets compiled via Webpack.
+ * Load theme assets compiled via Webpack.
  */
 function jln_child_theme_enqueue_assets() {
-	$parent_style_handle = 'twentytwentyfive-style';
-	$parent_stylesheet   = get_template_directory_uri() . '/style.css';
-	$parent_version      = wp_get_theme( 'twentytwentyfive' )->get( 'Version' );
-
-	wp_enqueue_style( $parent_style_handle, $parent_stylesheet, array(), $parent_version );
-
 	$child_css_path = get_stylesheet_directory() . '/css/style.css';
 	$child_css_uri  = get_stylesheet_directory_uri() . '/css/style.css';
 	$child_version  = file_exists( $child_css_path ) ? filemtime( $child_css_path ) : wp_get_theme()->get( 'Version' );
 
-	wp_enqueue_style( 'jong-literair-nederland-style', $child_css_uri, array( $parent_style_handle ), $child_version );
+	wp_enqueue_style( 'jong-literair-nederland-style', $child_css_uri, array(), $child_version );
 
 	$child_js_path = get_stylesheet_directory() . '/js/main.js';
 	if ( file_exists( $child_js_path ) ) {
